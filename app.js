e46a8897e972572af8943db553d911647d187c9a
krnewsome <kr.newsome21@yahoo.com>
2018-07-02 10:47:15 -0400

    refactored project

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/static', express.static('public'));
-
 app.set('view engine', 'pug');
 
+/*--------- ROUTES ----------*/
 const mainRoutes = require('./routes');
-
 
 app.use(mainRoutes);
 
+/*--------- ERRORS ----------*/
 app.use((req, res, next) => {
   const err = new Error('Sorry, Page Not Found =(...');
   err.status = 404;
@@ -26,6 +26,7 @@ app.use((err, req, res, next) => {
   res.status(err.status);
   res.render('error');
 })
+
 /*--------- SERVER ----------*/
 
 const port = 3000;
